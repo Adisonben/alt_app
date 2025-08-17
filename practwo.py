01faa75154466bcd40b8c5eb4c5997f5f8c2abe0
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-08-17 19:52:19 +0700

    ep29

---
+++ practwo.py
@@ -0,0 +1,46 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+Builder.load_file('style/radiobtn.kv')
+
+class MyLayout(Widget):
+    #pass
+    checks = []
+    # Checkbox
+    def checkbox_clicked(self, instance, value, content):
+        if value == True:
+            MyLayout.checks.append(content)
+        else:
+            MyLayout.checks.remove(content)
+        
+        if len(MyLayout.checks) > 0:
+            tops = ''
+            for x in MyLayout.checks:
+                tops = f'{tops} {x}'
+            self.ids.output_label.text = f'Selected: {tops}'
+        else:
+            self.ids.output_label.text = 'Please select a checkbox'
+
+    #slider
+    def slide_it(self, *args):
+        self.slide_text.text = str(int(args[1]))
+        self.slide_text.font_size = str(int(args[1]) * 5)
+
+    #image viewer & file chooser
+    def selected(self, filename):
+        try:
+            self.ids.my_image.source = filename[0]
+            print("Selected file: ", filename[0])
+
+        except:
+            pass
+
+class AwesomeApp(App):
+    def build(self):
+        Window.clearcolor = (0,0,0,1)
+        return MyLayout()
+    
+if __name__ == '__main__':
+    AwesomeApp().run()
