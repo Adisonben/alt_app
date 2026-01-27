ed5a251c11d002fd10e29e7d2037d5cb1a6d85f5
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-01-27 11:42:57 +0700

    Update test_match.py

--- test_match.py
+++ test_match.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import time
+from ctypes import c_ubyte
 
 from fingerprint_sdk.pysgfplib import PYSGFPLib
 from fingerprint_sdk.sgfdxdevicename import SGFDxDeviceName
 from fingerprint_sdk.sgfdxerrorcode import SGFDxErrorCode
-from ctypes import c_ubyte
 
 
 def check(result, step):
@@ -20,23 +20,35 @@ def main():
 
     sgfplib = PYSGFPLib()
 
+    # 1) Create
     check(sgfplib.Create(), "CreateSGFPMObject")
+
+    # 2) Init (ระบุรุ่นตรง ๆ)
     check(sgfplib.Init(SGFDxDeviceName.SG_DEV_FDU05), "Init")
 
-    print("👉 LED ON")
-    sgfplib.SetLedOn(True)
+    # 3) OpenDevice (ห้ามลืม)
+    check(sgfplib.OpenDevice(0), "OpenDevice")
 
+    # 4) LED ON
+    check(sgfplib.SetLedOn(True), "LED ON")
+
+    # Hamster Pro 20 = 260x300
     width, height = 260, 300
-    # image = bytearray(width * height)
     image = (c_ubyte * (width * height))()
 
     print("👉 Place finger on sensor")
     time.sleep(2)
+
+    # 5) Get Image
     check(sgfplib.GetImage(image), "GetImage")
 
+    # 6) LED OFF
     sgfplib.SetLedOn(False)
 
+    # 7) Close
+    check(sgfplib.CloseDevice(), "CloseDevice")
     check(sgfplib.Terminate(), "DestroySGFPMObject")
+
     print("=========== DONE ============")
 
 
