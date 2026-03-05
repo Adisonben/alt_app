35bf58471f605a6907ba28907748cfd9c257dac4
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-03-05 16:47:15 +0700

    fx

--- functions/audio.py
+++ functions/audio.py
@@ -1,22 +1,23 @@
 from kivy.core.audio import SoundLoader
+import threading
 
 _current_sound = None
 
 
-def play_sound(path):
+def _play(path):
     global _current_sound
 
-    try:
-        if _current_sound:
-            _current_sound.stop()
+    if _current_sound:
+        _current_sound.stop()
 
-        sound = SoundLoader.load(path)
+    sound = SoundLoader.load(path)
 
-        if sound:
-            _current_sound = sound
-            sound.play()
-        else:
-            print("Cannot load sound:", path)
+    if sound:
+        _current_sound = sound
+        sound.play()
 
-    except Exception as e:
-        print("Audio error:", e)
+
+def play_sound(path):
+    thread = threading.Thread(target=_play, args=(path,))
+    thread.daemon = True
+    thread.start()
--- pages/home.py
+++ pages/home.py
@@ -1,12 +1,8 @@
 from kivymd.uix.screen import MDScreen
 from functions.audio import play_sound
-from kivy.clock import Clock
 
 class Home(MDScreen):
     def on_enter(self):
-        Clock.schedule_once(self.play_welcome, 10)
-
-    def play_welcome(self, dt):
         play_sound("assets/sounds/voice_welcome.wav")
 
     def go_next(self):
