ae93bc1351891e77ab553e208d13d3716df98aee
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-03-05 16:56:07 +0700

    fx

--- pages/home.py
+++ pages/home.py
@@ -3,7 +3,7 @@ from functions.audio import play_sound
 
 class Home(MDScreen):
     def on_enter(self):
-        play_sound("assets/sounds/voice_welcome.wav")
+        play_sound("assets/sounds/voice_welcome.mp3")
 
     def go_next(self):
         self.manager.current = "employeeid"
