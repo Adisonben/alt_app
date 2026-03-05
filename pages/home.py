81be22d0e21af26a7cf480cea2f95c313e1bc5ee
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-03-05 16:24:08 +0700

    fx

--- pages/home.py
+++ pages/home.py
@@ -2,8 +2,8 @@ from kivymd.uix.screen import MDScreen
 from functions.audio import play_sound
 
 class Home(MDScreen):
-    # def on_enter(self):
-    #     play_sound("assets/sounds/voice_welcome.mp3")
+    def on_enter(self):
+        play_sound("assets/sounds/voice_welcome.wav")
 
     def go_next(self):
         self.manager.current = "employeeid"
