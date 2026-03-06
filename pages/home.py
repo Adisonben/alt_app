a0469f50b48d2a5afdb8928d0fa9a8586f3d7900
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-03-06 09:14:11 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -3,11 +3,14 @@ from functions.audio import pygame_play
 
 class Home(MDScreen):
     def __init__(self, **kwargs):
+        print("Home init")
         super().__init__(**kwargs)
         self.first_enter = True
-        
+
     def on_enter(self):
+        print("Home enter")
         if self.first_enter:
+            print("first enter triggleed")
             self.first_enter = False
             return
         pygame_play("assets/sounds/voice_welcome.mp3")
