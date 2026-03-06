c140db536a46e25312a70bcfeab72eea262d87da
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-03-06 09:12:08 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,14 @@ from kivymd.uix.screen import MDScreen
 from functions.audio import pygame_play
 
 class Home(MDScreen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.first_enter = True
+        
     def on_enter(self):
+        if self.first_enter:
+            self.first_enter = False
+            return
         pygame_play("assets/sounds/voice_welcome.mp3")
 
     def go_next(self):
